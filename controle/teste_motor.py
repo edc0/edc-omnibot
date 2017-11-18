5ecd610813e88cc5d94e0d171cced2dc5e4377e8
edc0 <emiliocan@gmail.com>
2017-11-18 18:57:15 -0200

    ooo

--- controle/teste_motor.py
+++ controle/teste_motor.py
@@ -6,41 +6,21 @@ GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output
 
 a = GPIO.PWM(24,100)               # pwm com 100 Hz no pino 24
-'''b = GPIO.PWM(23,100)
+#b = GPIO.PWM(23,100)
 
-a.start(0)
-b.start(0)
-
-try:
-    while True:
-
-        a.ChangeDutyCycle(10)
-        sleep(1000)
-        a.ChangeDutyCycle(50)
-        sleep(1000)
-        a.ChangeDutyCycle(100)
-        sleep(1000)
-        a.ChangeDutyCycle(10)
-        sleep(1000)
-        a.ChangeDutyCycle(0)
-        b.ChangeDutyCycle(30)
-        sleep(1000)
-        b.ChangeDutyCycle(95)
-        sleep(1000)
-        b.ChangeDutyCycle(0)
-        sleep(1000)
-'''
 a.start(1)
+#b.start(0)
+
 try:
     while True:
-        a.start(50)
-        #for dc in range (0,101,5):
-        #    a.ChangeDutyCycle(dc)
-        #    sleep(50)
-        #for dc in range (100,-1,5):
-        #    a.ChangeDutyCycle(dc)
-        #    sleep(50)
 
+        for dc in range (0,101,5):
+            a.ChangeDutyCycle(dc)
+            sleep(50)
+        for dc in range (100,-1,5):
+            a.ChangeDutyCycle(dc)
+            sleep(50)
+        
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
     a.stop()
     GPIO.cleanup()                 # resets all GPIO ports used by this program
