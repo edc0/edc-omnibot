866997e9c9cac7fb37e6273dec7e40f8d08c263c
edc0 <emiliocan@gmail.com>
2017-11-18 18:50:13 -0200

    mesma coisa, testando pwm

--- controle/teste_motor.py
+++ controle/teste_motor.py
@@ -5,8 +5,8 @@ GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output
 
-a = GPIO.PWM(24,100)               # pwm com 100 Hz no pino 24
-b = GPIO.PWM(23,100)
+a = GPIO.PWM(24,0.5)               # pwm com 100 Hz no pino 24
+'''b = GPIO.PWM(23,100)
 
 a.start(0)
 b.start(0)
@@ -29,6 +29,17 @@ try:
         sleep(1000)
         b.ChangeDutyCycle(0)
         sleep(1000)
+'''
+a.start(1)
+try:
+    while True:
+        for dc in range (0,101,5):
+            a.ChangeDutyCycle(dc)
+            sleep(50)
+        for dc in range (100,-1,5):
+            a.ChangeDutyCycle(dc)
+            sleep(50)
 
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
+    a.stop()
     GPIO.cleanup()                 # resets all GPIO ports used by this program
