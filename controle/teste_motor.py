1c493f9cb0b5b1ba2cc2e638f8fd26b2d0857f97
edc0 <emiliocan@gmail.com>
2017-11-18 19:16:28 -0200

    oooq

--- controle/teste_motor.py
+++ controle/teste_motor.py
@@ -5,24 +5,30 @@ GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output
 
-a = GPIO.PWM(24,100)               # pwm com 100 Hz no pino 24
-#b = GPIO.PWM(23,100)
+a = GPIO.PWM(24,400)               # pwm com 100 Hz no pino 24
+b = GPIO.PWM(23,100)
 
 a.start(1)
-#b.start(0)
 
 try:
     while True:
-        a.ChangeDutyCycle(20)
-        sleep(0.5)
-        a.ChangeDutyCycle(50)
-        sleep(0.5)
-        a.ChangeDutyCycle(100)
-        sleep(0.5)
-        a.ChangeDutyCycle(30)
-        sleep(0.5)
-        a.ChangeDutyCycle(0)
-        sleep(0.5)
+        a.start(1)
+        for dc in range (0,100,5):
+            a.ChangeDutyCycle(dc)
+            sleep(0.05)
+        for dc in range (100,0,5):
+            a.ChangeDutyCycle(dc)
+            sleep(0.01)
+        a.stop()
+        b.start(1)
+        for dc in range (0,100,5):
+            a.ChangeDutyCycle(dc)
+            sleep(0.05)
+        for dc in range (100,0,5):
+            a.ChangeDutyCycle(dc)
+            sleep(0.01)
+        b.stop()
+        sleep(1)
 
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
     a.stop()
