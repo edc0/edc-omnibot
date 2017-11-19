e6247bf6f17e9b4719217d84825562f7c1b4cf86
edc0 <emiliocan@gmail.com>
2017-11-19 18:17:51 -0200

    melhorando a resolução da rampa

--- controle/teste_motor.py
+++ controle/teste_motor.py
@@ -5,32 +5,32 @@ GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output
 
-a = GPIO.PWM(24,400)               # pwm com 100 Hz no pino 24
-b = GPIO.PWM(23,400)
+Ma = GPIO.PWM(24,400)               # pwm com 100 Hz no pino 24
+Mb = GPIO.PWM(23,400)
 
-a.start(1)
+Ma.start(1)
 
 try:
     while True:
-        a.start(1)
-        for dc in range (0,100,5):
-            a.ChangeDutyCycle(dc)
-            sleep(0.5)
+        Ma.start(1)
+        for dc in range (0,100,1):
+            Ma.ChangeDutyCycle(dc)
+            sleep(0.1)
         sleep(5)
-        for dc in range (100,0,-5):
-            a.ChangeDutyCycle(dc)
-            sleep(0.5)
-        a.stop()
+        for dc in range (100,0,-1):
+            Ma.ChangeDutyCycle(dc)
+            sleep(0.1)
+        Ma.stop()
         sleep(1)
-        b.start(1)
-        for dc in range (0,100,5):
-            b.ChangeDutyCycle(dc)
+        Mb.start(1)
+        for dc in range (0,100,1):
+            Mb.ChangeDutyCycle(dc)
             sleep(0.1)
         sleep(5)
-        for dc in range (100,0,-5):
-            b.ChangeDutyCycle(dc)
+        for dc in range (100,0,-1):
+            Mb.ChangeDutyCycle(dc)
             sleep(0.1)
-        b.stop()
+        Mb.stop()
         sleep(1)
 
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
