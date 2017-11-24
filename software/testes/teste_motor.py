dfbffaa6670aa82f3a7950846fb13e6de4cc78eb
edc0 <emiliocan@gmail.com>
2017-11-24 16:49:51 -0200

    testando PWMs em python com dois motores na plataforma montada

--- software/testes/teste_motor.py
+++ software/testes/teste_motor.py
@@ -2,40 +2,54 @@ import RPi.GPIO as GPIO            # import RPi.GPIO module
 from time import sleep             # lets us have a delay
 
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
-GPIO.setup(24, GPIO.OUT)           # set GPIO24 as an output
-GPIO.setup(23, GPIO.OUT)           # set GPIO23 as an output
-GPIO.setup(22, GPIO.IN)
-GPIO.setup(27, GPIO.IN)
+GPIO.setup(2,  GPIO.OUT)           # set GPIO24 as an output
+GPIO.setup(3,  GPIO.OUT)           # set GPIO23 as an output
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
 
-Ma = GPIO.PWM(24,400)               # pwm com 100 Hz no pino 24
-Mb = GPIO.PWM(23,400)
+M1a = GPIO.PWM(2,400)               # pwm com 100 Hz no pino 24
+M1b = GPIO.PWM(3,400)
 
-Ma.start(1)
+M2a = GPIO.PWM(17, 400)
+M2b = GPIO.PWM(27, 400)
+
+M1a.start(1)
+M2a.start(1)
 
 try:
     while True:
-        Ma.start(1)
+        M1a.start(1)
+        M2a.start(1)
         for dc in range (0,100,1):
-            Ma.ChangeDutyCycle(dc)
+            M1a.ChangeDutyCycle(dc)
+            M2a.ChangeDutyCycle(dc)
             sleep(0.1)
         sleep(5)
         for dc in range (100,0,-1):
-            Ma.ChangeDutyCycle(dc)
+            M1a.ChangeDutyCycle(dc)
+            M2a.ChangeDutyCycle(dc)
             sleep(0.1)
-        Ma.stop()
+        M1a.stop()
+        M2a.stop()
         sleep(1)
-        Mb.start(1)
+        M1b.start(1)
+        M2b.start(1)
         for dc in range (0,100,1):
-            Mb.ChangeDutyCycle(dc)
+            M1b.ChangeDutyCycle(dc)
+            M2b.ChangeDutyCycle(dc)
             sleep(0.1)
         sleep(5)
         for dc in range (100,0,-1):
-            Mb.ChangeDutyCycle(dc)
+            M1b.ChangeDutyCycle(dc)
+            M2b.ChangeDutyCycle(dc)
             sleep(0.1)
-        Mb.stop()
+        M1b.stop()
+        M2b.stop()
         sleep(1)
 
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
-    Ma.stop()
-    Mb.stop()
+    M1a.stop()
+    M2a.stop()
+    M1b.stop()
+    M2b.stop()
     GPIO.cleanup()                 # resets all GPIO ports used by this program
