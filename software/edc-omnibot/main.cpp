ae5f26a3e7a74e37d1e23b25cf0fab400df36580
edc0 <emiliocan@gmail.com>
2017-11-27 21:14:53 -0200

    teste dos motores com pigpio

--- software/edc-omnibot/main.cpp
+++ software/edc-omnibot/main.cpp
@@ -8,20 +8,52 @@
 #include "kinematics.h"
 
 double teste;
+int M1a, M1b, M2a, M2b, M3a, M3b;
+int E1a, E1b, E2a, E2b, E3a, E3b;
+int SP = 128;
+
+// setando pinos para os testes
+M1a = 2;
+M2a = 3;
 
 using namespace std;
 
+/* para encerrar com ctrl+c */
+// TODO: timeout
+void exit_from_key (int signum)
+{
+  cout << "Interrupt signal (" << signum << ") received.\n" ;
+  gpioTerminate(); // desliga motores
+  delay(100);
+  exit(signum);
+}
+
+void loop (void)
+{
+  if(SP > 0)
+  {
+    gpioPWM(M1a, 0);
+    gpioPWM(M1b, SP);
+  }
+  if(SP <= 0)
+  {
+    gpioPWM(M1a, SP);
+    gpioPWM(M2a, 0);
+  }
+}
+
+
 int main(void)
 {
-  teste = getRandom(0.5);
-  inverseKinematicsMobile();
-  cout << teste;
-  cout << "\n";
-  cout << omegapL;
-  cout << "\n";
-  omegapL = 85;
-  cout << omegapL;
-  cout << "\n";
+  signal(SIGINT, exit_from_key);
+  gpioInitialise();
+
+  // setando os pinos de saída:
+  gpioSetMode(M1a, PI_OUTPUT);
+  gpioSetMode(M2a, PI_OUTPUT);
+
+  // chama função loop() a cada 2ms
+  gpioSetTimerFunc(0, 2, loop);
 
   return 0;
 }
