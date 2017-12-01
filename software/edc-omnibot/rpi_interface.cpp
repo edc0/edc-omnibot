067fcafd03108e36ae97598cfcb1c74ff4b66573
edc0 <emiliocan@gmail.com>
2017-11-30 22:18:02 -0200

    ajsdgkjh

--- software/edc-omnibot/rpi_interface.cpp
+++ software/edc-omnibot/rpi_interface.cpp
@@ -14,7 +14,7 @@ OmniRPiInterface::OmniRPiInterface(int MA, int MB, int EA, int EB)
   Enc_A = EA;
   Enc_B = EB;
 
-  Kp = 0.5;
+  Kp = 1;
 }
 
 void OmniRPiInterface::resetPos()            // reinicia os contadores de posição
